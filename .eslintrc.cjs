1fef6dde3674a4fdf40114c28f0dc695b830570a
Ian Macalinao <ian@aptos.is>
2022-06-30 00:28:55 -0500

    fix lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,7 @@ require("@rushstack/eslint-patch/modern-module-resolution");
 
 /** @type import('eslint').Linter.Config */
 module.exports = {
-  extends: ["@saberhq/eslint-config"],
+  extends: ["@saberhq/eslint-config-react"],
   parserOptions: {
     project: "tsconfig.json",
   },
