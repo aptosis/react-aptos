f3d6b82d8793ab75bf50f917f060a7364a313dd4
Ian Macalinao <ian@aptos.is>
2022-06-30 13:36:15 -0500

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Node.js development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.simpleFlake {
+      inherit self nixpkgs;
+      name = "react-aptos-devenv";
+      shell = ./shell.nix;
+    };
+}
